99d5ee501f0cd0b493f05cc5defb155d7d4beb31
Julian <juliandemourgues@gmail.com>
2016-10-15 10:12:56 +0200

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,9 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
        return (
-       <div>
-        <h3>Examples Component</h3>
-      </div>
+        <div>
+          <h3>Examples Component</h3>
+          <p>Welcome to Github</p>
+        </div>
          )
 };
 
--- public/bundle.js
+++ public/bundle.js
@@ -26582,15 +26582,20 @@
 	var React = __webpack_require__(1);
 	
 	var Examples = function Examples(props) {
-	       return React.createElement(
-	              'div',
-	              null,
-	              React.createElement(
-	                     'h3',
-	                     null,
-	                     'Examples Component'
-	              )
-	       );
+	  return React.createElement(
+	    'div',
+	    null,
+	    React.createElement(
+	      'h3',
+	      null,
+	      'Examples Component'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to Github'
+	    )
+	  );
 	};
 	
 	module.exports = Examples;
